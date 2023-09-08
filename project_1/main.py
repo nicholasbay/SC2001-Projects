a8a80dd5222f9e1e77bbc2390a400db06e57b70b
nicholasbay <bay.yong.wei@gmail.com>
2023-09-08 16:02:15 +0800

    Updated main.py
    generate_dataset() now gives and array filled with unique elements
    
    insertion_sort() now implemented and working

--- project_1/main.py
+++ project_1/main.py
@@ -1,41 +1,55 @@
 import constant
-from random import randint
+import random
 
-# To generate dataset of size n
+# To generate dataset (without duplicates) of size n
 def generate_dataset(n):
-    dataset = []
-    for i in range(n):
-        dataset.append(randint(1, n+1))
-    return dataset
+    # Create array of length n, consisting of unique elements from 1 to n inclusive
+    arr = [x for x in range(1, n+1)]
+    # Suffle array order
+    random.shuffle(arr)
+    return arr
 
 
 # Insertion sort
-def insertion_sort(dataset):
-    # insertion sort logic
+def insertion_sort(arr, n):
+    for i in range(n):
+        for j in range(i, 0, -1):
+            if (arr[j] < arr[j-1]):
+                tmp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = tmp
+            else:
+                break
 
 
-# Merge sort
-def merge_sort(dataset):
+# Mergesort
+#def merge_sort(arr, n):
     # merge sort logic
+
+
+# Hybrid sort (merge & insertion)
+#def hybrid_sort(arr, n):
+    # merge sort for n > S, insertion sort for n < S
 
 
 # Driver function
 def main():
     # Generate random dataset of size n
-    data = generate_dataset(constant.n)
+    dataset = generate_dataset(constant.n)
     
+    '''
     # Time measurement from here?
     if constant.n > constant.S:
-        merge_sort(data)
+        merge_sort(dataset, constant.n)
     else:
-        insertion_sort(data)
+        insertion_sort(dataset, constant.n)
     # End time measurement here?
+    '''
 
     # Output sorted dataset
-    for i in data:
-        print(i)
+    for i in range(len(dataset)):
+        print(dataset[i])
 
 
 if __name__ == '__main__':
     main()
-
