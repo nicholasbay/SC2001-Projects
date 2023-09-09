1b002d7e11f32e36a21a962bf63ccd5b98e3d11f
l3ryanchia <128674012+l3ryanchia@users.noreply.github.com>
2023-09-09 12:20:03 +0800

    Update merge_sort

--- project_1/main.py
+++ project_1/main.py
@@ -23,8 +23,33 @@ def insertion_sort(arr, n):
 
 
 # Mergesort
-#def merge_sort(arr, n):
-    # merge sort logic
+def merge_sort(arr):
+    mid = len(arr)/2
+    
+    if len(arr) <= 1:
+        return arr
+    else:
+        left_half = merge_sort(arr[:mid])
+        right_half = merge_sort(arr[mid:])
+        
+    return merge(left_half, right_half)
+    
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+    
+    while (left_index < len(left)) and (right_index < len(right)):
+        if (left[left_index] < right[right_index]):
+            result.append(left[left_index])
+            left_index += 1;
+        else:
+            result.append(right[right_index])
+            right_index += 1;
+            
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+
+    return result
 
 
 # Hybrid sort (merge & insertion)
